57eb819fa7dde3dbea681d0f1d91ddbe3d69e279
preeti tambe <preetitambe158@gmail.comgit config --global user.name priti>
2024-02-10 16:26:07 +0530

    Created Three Servlet Pages

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -25,6 +26,25 @@ public class LoginServlet extends HttpServlet {
 		PrintWriter out=response.getWriter();
 		
 		request.getRequestDispatcher("link.html").include(request, response);
+		
+		String name=request.getParameter("uname");
+		String pass=request.getParameter("upass");
+		
+		if(pass.equals("123"))
+		{
+			out.print("<br>You are successfully Logged In");
+			out.print("<br> <b>Welcome</b> "+name);
+			
+			Cookie ck= new Cookie("uname", name);
+			response.addCookie(ck);
+			
+		}else
+		{
+			out.print("<br><br>SORRY, Username or password error...!!");
+			request.getRequestDispatcher("login.html").include(request, response);
+		}
+		out.close();
+		
 		
 	}
 
